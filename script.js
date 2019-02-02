78894be5a82ade95e394ddc6c04276af381437f5
Vinicius Donadon <32843269+vbdonadon@users.noreply.github.com>
2019-02-01 22:56:51 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Crie um script em uma página hospedada no github onde que exiba 6 fotos obtidas através
 // da API do Flickr. Informe a URL para o exemplo que criou.
-const url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=f2d4b829e1d5a0f551e9ef054d756f33&user_id=167980004%40N05&format=json&nojsoncallback=1&auth_token=72157706388893944-f5a1d8d8c0e69e70&api_sig=1a345eb70363bc4eadff8dfc3eebf7b2'
+const url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=e0704084a765455afa2c7af759082590&user_id=167980004%40N05&format=rest&auth_token=72157704865735461-4bb18f0201f42de3&api_sig=cd02ecfd7642bbd4eccd0a172aedff50'
 $(document).ready(function () {
     $.ajax({
         url: url,
