c21ae035dce3dd7d211a54ddcd6212ad961900b4
Vinicius Donadon <biumdonadon@hotmail.com>
2019-02-03 16:41:41 -0200

    trying to solve the key expired error

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Crie um script em uma página hospedada no github onde que exiba 6 fotos obtidas através
 // da API do Flickr. Informe a URL para o exemplo que criou.
-const url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=e0704084a765455afa2c7af759082590&user_id=167980004%40N05&format=json&nojsoncallback=1&auth_token=72157704865735461-4bb18f0201f42de3&api_sig=1eb85e1c8f5d4713785aa77c00c12784'
+const getFrob = 'https://api.flickr.com/services/rest/?method=flickr.auth.getFrob&api_key=e0704084a765455afa2c7af759082590&format=json&nojsoncallback=1&auth_token=72157704865735461-4bb18f0201f42de3&api_sig=447035ef857ed335d2241d9725107798'
+const url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=52cad1a1c4789a58813e763039891e0e&user_id=167980004%40N05&format=json&nojsoncallback=1&auth_token=72157706457922534-e03993ad27d8739e&api_sig=ad58f732e1b885304a25d98689a9e915'
 $(document).ready(function () {
     $.ajax({
         url: url,
