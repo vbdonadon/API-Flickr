b965d46a9a24a06ac74eefbe760f23d1e01baf1e
Vinicius Donadon <vinicius.donadon@sunsetcom.com.br>
2019-02-04 09:58:07 -0200

    New key

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Crie um script em uma página hospedada no github onde que exiba 6 fotos obtidas através
 // da API do Flickr. Informe a URL para o exemplo que criou.
-const getFrob = 'https://api.flickr.com/services/rest/?method=flickr.auth.getFrob&api_key=e0704084a765455afa2c7af759082590&format=json&nojsoncallback=1&auth_token=72157704865735461-4bb18f0201f42de3&api_sig=447035ef857ed335d2241d9725107798'
-const url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=52cad1a1c4789a58813e763039891e0e&user_id=167980004%40N05&format=json&nojsoncallback=1&auth_token=72157706457922534-e03993ad27d8739e&api_sig=ad58f732e1b885304a25d98689a9e915'
+const url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=ec5ed7f7ffca85d7347031e853bf38eb&user_id=167980004%40N05&format=json&nojsoncallback=1&auth_token=72157678363201418-2f8dd87178a3dc5e&api_sig=44b183a5169b1802835a72006291036b'
 $(document).ready(function () {
     $.ajax({
         url: url,
